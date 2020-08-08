a11141a49a2b440bc8e2239db79b6206847417ca
Emma Dawson <emma.l.dawson@gmail.com>
2020-08-08 21:17:30 +0200

    Create callbacks ready for animating the timer

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 class Timer {
-    constructor(durationInput, startButton, pauseButton) {
+    constructor(durationInput, startButton, pauseButton, callbacks) {
         this.durationInput = durationInput;
         this.startButton = startButton;
         this.pauseButton = pauseButton;
+        if (callbacks) {
+            this.onStart = callbacks.onStart;
+            this.onTick = callbacks.onTick;
+            this.onComplete = callbacks.onComplete;
+        }
 
         this.startButton.addEventListener("click", this.start);
         this.pauseButton.addEventListener("click", this.pause);
     }
     start = () => {
+        if (this.onStart) {
+            this.onStart();
+        }
         this.tick();
         this.interval = setInterval(this.tick, 1000);
     };
@@ -17,8 +25,26 @@ class Timer {
     }
 
     tick = () => {
-        const timeRemaining = parseFloat(this.durationInput.value);
-        this.durationInput.value = timeRemaining - 1;
+        if (this.timeRemaining <= 0) {
+            this.pause();
+            if (this.onComplete) {
+                this.onComplete();
+            }
+        } else {
+            this.timeRemaining = this.timeRemaining - 1;
+            if (this.onTick) {
+                this.onTick();
+            }
+        }
+        
+    }
+
+    get timeRemaining() {
+        return parseFloat(this.durationInput.value);
+    }
+
+    set timeRemaining(time) {
+        this.durationInput.value = time;
     }
 }
 
@@ -26,4 +52,14 @@ const durationInput = document.querySelector("#duration");
 const startButton = document.querySelector("#start");
 const pauseButton = document.querySelector("#pause");
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart() {
+        console.log("timer started");
+    },
+    onTick() {
+console.log("timer ticked");
+    },
+    onComplete() {
+console.log("completed");
+    }
+});
